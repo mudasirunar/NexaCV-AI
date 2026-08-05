4d39325b3ac09315b81e621a9b483ed92ca0caa9
Mudasir Ali <unarmudasir@gmail.com>
2026-08-06 01:39:37 +0500

    fix: update build.gradle.kts to adjust release build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,10 +23,12 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            //isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            //signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
