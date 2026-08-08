76090c67396fe95d2daaba1952b2f3cfe9e6e9b8
Mudasir Ali <unarmudasir@gmail.com>
2026-08-09 03:41:51 +0500

    feat: update version code to 4 and version name to 0.2.1; enable BuildConfig generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mudasir.nexacvai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -40,6 +40,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
