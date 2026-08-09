c03beb7754596dbe800460a5f4ebe5689320b95f
Mudasir Ali <unarmudasir@gmail.com>
2026-08-10 04:01:48 +0500

    chore: update version code to 5 and version name to 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mudasir.nexacvai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
