fb213acbceb3921d526ccad7a9a460a944216faf
Mudasir Ali <unarmudasir@gmail.com>
2026-08-11 03:38:04 +0500

    feat: bump version code to 6 and update version name to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mudasir.nexacvai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
